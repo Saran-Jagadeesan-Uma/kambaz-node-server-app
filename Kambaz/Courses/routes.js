d5c392fca78d8b4cc70296300368cc5e596e2cf0
Saran J U <122967632+Saran-Jagadeesan-Uma@users.noreply.github.com>
2025-06-17 01:47:48 +0530

    sumamry

--- Kambaz/Courses/routes.js
+++ Kambaz/Courses/routes.js
@@ -3,41 +3,62 @@ import * as modulesDao from "../Modules/dao.js";
 
 export default function CourseRoutes(app) {
   app.get("/api/courses", async (req, res) => {
-    const courses = await dao.findAllCourses();
-    res.json(courses);
+    try {
+      const courses = await dao.findAllCourses();
+      res.json(courses);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
   });
 
   app.post("/api/courses", async (req, res) => {
-    const newCourse = await dao.createCourse(req.body);
-    res.json(newCourse);
+    try {
+      const newCourse = await dao.createCourse(req.body);
+      res.json(newCourse);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
   });
 
   app.get("/api/courses/:courseId/modules", async (req, res) => {
-    const { courseId } = req.params;
-    const modules = await modulesDao.findModulesForCourse(courseId);
-    res.json(modules);
+    try {
+      const { courseId } = req.params;
+      const modules = await modulesDao.findModulesForCourse(courseId);
+      res.json(modules);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
   });
 
   app.post("/api/courses/:courseId/modules", async (req, res) => {
-    const { courseId } = req.params;
-    const module = {
-      ...req.body,
-      course: courseId,
-    };
-    const newModule = await modulesDao.createModule(module);
-    res.send(newModule);
+    try {
+      const { courseId } = req.params;
+      const module = { ...req.body, course: courseId };
+      const newModule = await modulesDao.createModule(module);
+      res.json(newModule);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
   });
 
   app.delete("/api/courses/:courseId", async (req, res) => {
-    const { courseId } = req.params;
-    const status = await dao.deleteCourse(courseId);
-    res.send(status);
+    try {
+      const { courseId } = req.params;
+      const status = await dao.deleteCourse(courseId);
+      res.send(status);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
   });
 
   app.put("/api/courses/:courseId", async (req, res) => {
-    const { courseId } = req.params;
-    const courseUpdates = req.body;
-    const status = await dao.updateCourse(courseId, courseUpdates);
-    res.send(status);
+    try {
+      const { courseId } = req.params;
+      const courseUpdates = req.body;
+      const status = await dao.updateCourse(courseId, courseUpdates);
+      res.send(status);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
   });
 }
