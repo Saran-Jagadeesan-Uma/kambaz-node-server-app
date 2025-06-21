af663e1a5efe8a7b49cefbf90a29b1701635ec60
Sai Rashwant <sairashwantno1@gmail.com>
2025-06-21 08:56:48 -0400

    Fixed quizzes error

--- Kambaz/Questions/dao.js
+++ Kambaz/Questions/dao.js
@@ -3,12 +3,19 @@ import QuizModel from "../Quizzes/model.js";
 
 export const createQuestion = async (req, res) => {
   const { quizId } = req.params;
+
   try {
+    // Remove quiz field from body if present
+    const questionData = { ...req.body };
+    delete questionData.quiz;
+
+    // Create question with quizId from params
     const newQuestion = await QuestionModel.create({
-      ...req.body,
+      ...questionData,
       quiz: quizId,
     });
 
+    // Link question ID to the quiz document
     await QuizModel.findByIdAndUpdate(quizId, {
       $push: { questions: newQuestion._id },
     });
@@ -24,6 +31,7 @@ export const findQuestionsForQuiz = async (req, res) => {
   const { quizId } = req.params;
   try {
     const quiz = await QuizModel.findById(quizId).populate("questions");
+    if (!quiz) return res.status(404).send("Quiz not found");
     res.json(quiz.questions);
   } catch (err) {
     res.status(500).send("Error fetching questions");
